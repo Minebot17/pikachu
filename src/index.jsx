bc4b225cbe48638338b39ace5c8d7f6ee35f8620
Eugene <serpinin@mail.ru>
2020-01-11 17:55:57 +0400

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/base.scss';
+import Button from 'react-bootstrap/Button';
 
 ReactDOM.render(
     <div>
-        <p>Test</p>
+        <Button variant="primary">Primary</Button>
     </div>
     , document.getElementById("react")
 );
